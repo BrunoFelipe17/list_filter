5bcb1704c9a7489b786752c481127039c63bd912
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-07-09 18:30:54 -0300

    Count the odd numbers in a list

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,26 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) do
+    filter(list)
   end
+
+  defp filter(list) do
+    list
+    |> Enum.map(&parse_int/1)
+    |> Enum.filter(fn elem -> rem(elem, 2) == 1 end)
+    |> length
+  end
+
+  defp parse_int(elem) do
+    Integer.parse(elem)
+    |> check_number()
+  end
+
+  defp check_number({number, _}) do
+    number
+  end
+
+  defp check_number(:error) do
+    0
+  end
+
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,11 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "returns the length of odd numbers" do
+      assert ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"]) == 3
+      assert ListFilter.call(["2", "4", "6"]) == 0
+      assert ListFilter.call(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]) == 5
+    end
   end
 end
